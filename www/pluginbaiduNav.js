419eb1c4f466ad48a6e9e67f933f0c68ce77d942
yanslat <yanslat@163.com>
2016-03-31 22:54:45 +0800

    modify pluginbaiduNav

--- www/pluginbaiduNav.js
+++ www/pluginbaiduNav.js
@@ -1,5 +1,5 @@
 var exec = require('cordova/exec');
 
 exports.coolMethod = function(arg0, success, error) {
-    exec(success, error, "plugin-baiduNav", "coolMethod", [arg0]);
+    exec(success, error, "pluginbaiduNav", "coolMethod", [arg0]);
 };
