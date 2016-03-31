bf3f70618405ed762914d5ef056dfd7fb15a33c1
yanslat <yanslat@163.com>
2016-03-31 22:21:33 +0800

    修改src plugin

--- src/android/pluginbaiduNav.java
+++ src/android/pluginbaiduNav.java
@@ -5,21 +5,35 @@ import org.apache.cordova.CallbackContext;
 import org.apache.cordova.LOG;
 import org.apache.cordova.PluginResult;
 
-import android.app.Activity;
-import android.content.Intent;
-import android.os.Bundle;
-import android.util.Log;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.res.Configuration;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Handler;
+import android.view.Display;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup.LayoutParams;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
 
 /**
  * This class echoes a string called from JavaScript.
  */
-public class pluginbaiduNav extends CordovaPlugin {
+public class plugin-baiduNav extends CordovaPlugin {
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
@@ -27,17 +41,18 @@ public class pluginbaiduNav extends CordovaPlugin {
            // String message = args.getString(0);
             //this.coolMethod(message, callbackContext);
             //调起百度地图客户端
-			try {
-			     Intent intent = Intent.getIntent("intent://map/direction?origin=latlng:34.264642646862,108.95108518068|name:我家&destination=大雁塔&mode=driving®ion=西安&referer=Autohome|GasStation#Intent;scheme=bdapp;package=com.baidu.BaiduMap;end");
-			         if(isInstallByread("com.baidu.BaiduMap")){
-			             this.cordova.startActivity(intent); //启动调用
-			             Log.e("GasStation", "百度地图客户端已经安装") ;
-			         }else{
-			             Log.e("GasStation", "没有安装百度地图客户端") ;
-			         }
-			     } catch (URISyntaxException e) {
-			         e.printStackTrace();
-			}
+            try {
+                 Intent intent = Intent.getIntent("intent://map/direction?origin=latlng:34.264642646862,108.95108518068|name:我家&destination=大雁塔&mode=driving®ion=西安&referer=Autohome|GasStation#Intent;scheme=bdapp;package=com.baidu.BaiduMap;end");
+                     cordova.getActivity().startActivity(intent); //启动调用
+                     // if(isInstallByread("com.baidu.BaiduMap")){
+                     //     this.cordova.startActivity(intent); //启动调用
+                     //     Log.e("GasStation", "百度地图客户端已经安装") ;
+                     // }else{
+                     //     Log.e("GasStation", "没有安装百度地图客户端") ;
+                     // }
+                 } catch (Exception e) {
+                     e.printStackTrace();
+            }
             return true;
         }
         return false;
